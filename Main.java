a9cbf9877c423bfb2994dea10678c3f0fd55957a
AAlimov <Andrei.Alimov@endava.com>
2020-10-26 19:41:39 +0200

    Added methods 3&4

--- Main.java
+++ Main.java
@@ -2,4 +2,12 @@ public class Main {
     //Created on main branch
     //Changed on Branch1
     //Changed on Branch2
+
+    public void method3() {
+        //Added on Branch2
+    }
+
+    public void method4() {
+        //Added on Branch2
+    }
 }
